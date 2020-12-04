16e69082aa9576858d2d663328a21bafc2e06c49
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2020-12-04 14:22:01 -0300

    Software final

---
+++ Mvc_login/Models/AccountViewModels.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mvc_login.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+    }
+
+    public class ManageUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "O campo {0} deve ter no minimo {2} caracteres.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Senha")]
+        [Compare("NewPassword", ErrorMessage = "A nova senha e confirmação de senha não correspondem.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        [Display(Name = "Lembrar?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "O campo {0} deve ter no minimo {2} caracters", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Senha")]
+        [Compare("Password", ErrorMessage = "A Senha e a Confirmação de senha são diferentes.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
